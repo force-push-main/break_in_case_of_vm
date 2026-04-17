3af5e19f31bd78fb93bebfd313b25a762a4c55a1
force-push-main <10535@holbertonstudents.com>
2026-04-17 17:14:06 +1000

    fixed file path issues with setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-USER=whoami
+sudo apt install curl
 
 ##### nvim setup ######
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo >> /home/${USER}/.bashrc
-echo 'eval :$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"' >> /home/${USER}/.bashrc
+echo >> "${HOME}/.bashrc"
+echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"' >> "${HOME}/.bashrc"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
 sudo apt-get install build-essential
-exec $SHELL 
 brew install gcc
 brew install ripgrep
 brew install fd
 brew install neovim
+[ ! -d "${HOME}/.config/nvim" ] && mkdir "${HOME}/.config/nvim"
 shopt -s extglob
 mv !(setup.sh) ~/.config/nvim
 
 #### kitty setup #####
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+sudo apt install kitty
 git clone https://github.com/force-push-main/kitty_config.git ~/.config/kitty
-exec $SHELL
+kitty
